ae65301b9da09713e32a0a11cd29751cd3b3d279
Scvpn <154565664+Scvpn@users.noreply.github.com>
2024-01-10 22:23:39 +0800

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -108,8 +108,8 @@ clear
 ###########- END COLOR CODE -##########
 tram=$( free -h | awk 'NR==2 {print $2}' )
 uram=$( free -h | awk 'NR==2 {print $3}' )
-ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
-CITY=$(curl -s ipinfo.io/city )
+ISP=$(cat /root/.isp)
+CITY=$(cat /root/.city)
 # Getting CPU Information
 cpu_usage1="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum}')"
 cpu_usage="$((${cpu_usage1/\.*} / ${corediilik:-1}))"
